9e2eea9017b4159c063e19f7fb6c388d64e174b3
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2019-03-31 20:41:42 +0200

    Initial commit

---
+++ route-planner-server/src/main/java/edu/route/planner/RoutePlannerServerApplication.java
@@ -0,0 +1,13 @@
+package edu.route.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutePlannerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutePlannerServerApplication.class, args);
+    }
+
+}
