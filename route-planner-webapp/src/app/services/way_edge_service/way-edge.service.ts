c0049c7ab07e9c1fc4ab2b155f98bc7897a663eb
Bartłomiej Bielecki <bartlomiejbielecki1@gmail.com>
2019-05-30 18:17:45 +0200

    WIP - algorithm must return collection of WayEdges Write query to get WatEdge by ID, transform Edge from algorithm into WayEdge and return the whole list.

--- route-planner-webapp/src/app/services/way_edge_service/way-edge.service.ts
+++ route-planner-webapp/src/app/services/way_edge_service/way-edge.service.ts
@@ -16,13 +16,13 @@ export class WayEdgeService {
     return this.http.get<WayEdge>('/api/wayEdges/direct', { params: params });
   }
 
-  findRouterOptimalPath(source: number, destination: number, distanceBuffer: number, durationBuffer: number): Observable<WayEdge> {
+  findRouterOptimalPath(source: number, destination: number, distanceBuffer: number, durationBuffer: number): Observable<WayEdge[]> {
     let params = new HttpParams()
       .set("source", source.toString())
       .set("destination", destination.toString())
       .set("distanceBuffer", distanceBuffer.toString())
       .set("durationBuffer", durationBuffer.toString());
-    return this.http.get<WayEdge>('/api/wayEdges/optimal/router', { params: params });
+    return this.http.get<WayEdge[]>('/api/wayEdges/optimal/router', { params: params });
   }
 
   findAll(): Observable<WayEdge[]> {
